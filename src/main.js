a35f4fd9ac7648b9b25f15dce16ea95260d21e9a
saeheee <saehee15@gmail.com>
2021-12-09 17:09:32 +0900

    20211209_예시

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//특정한 폴더에 index파일은 생략가능 밑에 /index.js 생략가능
 import router from './routes/index.js'
+import store from './store/index.js'
 
 createApp(App)
     .use(router)
+    .use(store)
     .mount('#app')
